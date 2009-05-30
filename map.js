357b228cec4d547d72f3f5c8843ccf391509148e
Daniel Morrison <daniel@collectiveidea.com>
2009-05-30 16:02:48 -0400

    Refactored the code a bit, and added a few notes to the readme.

--- map.js
+++ map.js
@@ -1,74 +1,86 @@
-var infoTemplate = new Template('<b><a href="#{urlList[0]}">#{fn}</a></b><br/>');
-var addressTemplate = new Template('#{streetAddress}<br/>#{locality}, #{region} #{postalCode} #{country}');
-
 var Map = {
+  infoWindowContent: function(hcard) {
+    var address = hcard.adrList[0]
+    return '<b>'
+      +(hcard.urlList ? '<a href="'+hcard.urlList[0]+'">'+hcard.fn+'</a>' : hcard.fn)
+      +'</b><br/>'
+      +address.streetAddress
+      +'<br/>'
+      +address.locality+', '+address.region+' '+address.postalCode+' '+address.country
+  },
+  
   showPoint: function(hcard) {
     var point = new google.maps.LatLng(hcard.geo.latitude, hcard.geo.longitude);
     var marker = new google.maps.Marker({
-          position: point, 
-          map: Map.map, 
-          title: hcard.fn,
-          icon: colored_pin((hcard.categoryList ? hcard.categoryList[0] : 'red')),
-          shadow: shadow(),
-          shape: {
-                coord: [1, 1, 1, 20, 18, 20, 18 , 1],
-                type: 'poly'
-            }
-      });
+        position: point, 
+        map: Map.map, 
+        title: hcard.fn,
+        icon: Map.coloredPin((hcard.categoryList ? hcard.categoryList[0] : 'red')),
+        shadow: Map.shadow(),
+        shape: { coord: [1, 1, 1, 20, 18, 20, 18 , 1], type: 'poly'}
+    });
     
-    var infowindow = new google.maps.InfoWindow({
-      content: infoTemplate.evaluate(hcard) + addressTemplate.evaluate(hcard.adrList[0]),
+    var infoWindow = new google.maps.InfoWindow({
+      content: Map.infoWindowContent(hcard),
       size: new google.maps.Size(250,50)
     });
     
     google.maps.event.addListener(marker, 'click', function() {
-      infowindow.open(Map.map, marker);
+      infoWindow.open(Map.map, marker);
     });
     
     Map.bounds.extend(point);    
   },
   
+  hcards: function() {
+    if (!Map._hcards) {
+      Map._hcards = HCard.discover().select(function(card) { return card.geo })
+    }
+    return Map._hcards;
+  },
+  
+  domId: 'map',
+  
+  options: {
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  },
+  
   display: function() {
-    var hcards = HCard.discover().select(function(card) { return card.geo });
-    if (hcards.length == 0) return
+    if (Map.hcards().length == 0) return
 
-    var center = new google.maps.LatLng(hcards[0].geo.latitude, hcards[0].geo.longitude);
-
-    var mapOptions = {
-      zoom: 8,
-      center: center,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    Map.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    Map.map = new google.maps.Map(document.getElementById(Map.domId), Map.options);
 
     // Map.map.enableScrollWheelZoom();
     // Map.map.addControl(new GOverviewMapControl());
 
+    var center = new google.maps.LatLng(Map.hcards()[0].geo.latitude, Map.hcards()[0].geo.longitude);
     Map.bounds = new google.maps.LatLngBounds(center);
-    hcards.each(Map.showPoint);
+    
+    Map.hcards().forEach(Map.showPoint); // forEach is defined in microformat.js
 
     // Fit all points in view
     Map.map.fitBounds(Map.bounds);
-  }
-}
-
-icons = {}
-
-function colored_pin(color) {
-  if (!icons[color]) {
-    icons[color] = new google.maps.MarkerImage("http://labs.google.com/ridefinder/images/mm_20_"+(color || 'red')+".png",
-      new google.maps.Size(12, 20),
-      new google.maps.Point(0,0),
-      new google.maps.Point(6,20));
-  }
-  return icons[color];
-}
-
-function shadow() {
+  },
+  
+  icons: {},
+  
+  coloredPin: function(color) {
+    if (!Map.icons[color]) {
+      Map.icons[color] = new google.maps.MarkerImage("http://labs.google.com/ridefinder/images/mm_20_"+(color || 'red')+".png",
+        new google.maps.Size(12, 20),
+        new google.maps.Point(0,0),
+        new google.maps.Point(6,20));
+    }
+    return Map.icons[color];
+  },
+  
+  shadow: function() {
     return new google.maps.MarkerImage("http://labs.google.com/ridefinder/images/mm_20_shadow.png",
       new google.maps.Size(22, 20),
       new google.maps.Point(5,1),
-      new google.maps.Point(0,20));      
+      new google.maps.Point(0,20));          
+  }
 }
 
 // Dunno why IE doesn't like dom:loaded.
